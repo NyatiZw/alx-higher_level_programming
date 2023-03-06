fb60243851c484a24bb56e487d5eb4652cd3a9d5
NyatiZw <appsmaid19@gmail.com>
2023-03-06 05:41:19 -0800

    python print on stdout

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,24 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer member
+ * @next: pointer to next node
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const list_t *h);
+listint_t *add_nodeint(listint_t **heaad, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* _LISTS_H */
