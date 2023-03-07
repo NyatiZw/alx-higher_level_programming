183b9ba19d2b18e12b51fe1916114a13b2ba2d1f
NyatiZw <appsmaid19@gmail.com>
2023-03-07 20:38:18 +0000

    print number combination in python

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for num1 in range(0, 10):
+    for num2 in range(num1 + 1, 10):
+        if (num1 != num2 and num2 != num1 or num1 == num2, num2 == num1):
+            print("{} {}".format(num1, num2), end=", ")
