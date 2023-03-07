8873db82e2defcbbdee79d3d201e95651d981509
NyatiZw <appsmaid19@gmail.com>
2023-03-07 19:38:53 +0000

    print combination

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    print("{:02}".format(int(num)), end=", ")
+    if num == 99:
+        print("{}".format(int(num)))
+    else:
+        print("{:02}".format(int(num)), end=", ")
