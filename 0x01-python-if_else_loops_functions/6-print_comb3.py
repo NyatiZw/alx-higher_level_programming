3c512a612208974a64707d2aa9083a607af48942
NyatiZw <appsmaid19@gmail.com>
2023-03-08 06:44:06 +0000

    corrections

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for num1 in range(0, 10):
     for num2 in range(num1 + 1, 10):
         if (num1 != num2 and num2 != num1 or num1 == num2, num2 == num1):
-            print("{} {}".format(num1, num2), end=", ")
+            print("{}{}".format(num1, num2), end=", ")
