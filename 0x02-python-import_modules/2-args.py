6a2bc9509e7f1b3392b8de9b9186dd241aff251f
NyatiZw <appsmaid19@gmail.com>
2023-03-10 11:35:31 +0000

    python command line commands

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-n = len(sys.argv)
+if __name__ == "__main__":
 
-if (n == 1):
-    print("0 arguments.")
+    n = len(sys.argv)
 
-for i in range(1, n):
-    if (n >= 2):
-        print('{} arguments'.format(i, end=" "))
-    elif (n == 1):
-        print('{} argument'.format(i, end=" "))
+    if (n == 1):
+        print("0 arguments.")
+
+    for i in range(1, n):
+        if (n >= 2):
+            print('{} arguments'.format(i, end=" "))
+        elif (n == 1):
+            print('{} argument'.format(i, end=" "))
