dfd84d65df0e359499a3a72e5c2fc95d263ce172
NyatiZw <appsmaid19@gmail.com>
2023-03-10 10:42:51 +0000

    importing modules in python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,9 @@ n = len(sys.argv)
 
 if (n == 1):
     print("0 arguments.")
-elif (n > 1):
-    for i in range(1, n):
-        print('{}'.format(i, end=" "))
 
+for i in range(1, n):
+    if (n >= 2):
+        print('{} arguments'.format(i, end=" "))
+    elif (n == 1):
+        print('{} argument'.format(i, end=" "))
