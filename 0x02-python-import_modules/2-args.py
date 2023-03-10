8faffbf54d75985ad3f2195bda19b17e048efd79
NyatiZw <appsmaid19@gmail.com>
2023-03-10 01:15:15 +0000

    print number of command line args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,9 @@ import sys
 
 n = len(sys.argv)
 
-for i in range(1, n):
-    print('{}'.format(sys.argv[i], end=" "))
+if (n == 1):
+    print("0 arguments.")
+else:
+    for i in range(1, n):
+        print('{}'.format(sys.argv[i], end=" "))
+
