46f3b06dc6088b714d9cad4de49e344a29164049
NyatiZw <appsmaid19@gmail.com>
2023-03-14 00:42:35 +0000

    remove all letter c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-
-temp = [x for x in my_string if x != 'c' and x != 'C']
-return("".join(temp))
+    temp = [x for x in my_string if x != 'c' and x != 'C']
+    return("".join(temp))
