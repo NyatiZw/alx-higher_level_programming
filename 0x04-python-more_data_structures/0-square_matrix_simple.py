a2c9473bcd23885200377cc6a8e45eab03cb03d4
NyatiZw <appsmaid19@gmail.com>
2023-03-18 22:00:25 -0700

    0.Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-# Function to compute the sqaure value of all integers of a matrix
+#Function to compute the sqaure value of all integers of a matrix
 def sqaure_matrix_simple(matrix=[]):
-    result = matrix.copy()
+    result = matrix
 
     for i in range(len(matrix)):
         result[i] = list(map(lambda x: x**2, matrix[i]))
