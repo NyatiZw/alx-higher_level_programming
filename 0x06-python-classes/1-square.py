98d68c2492dfea966659246217ec95b7b90f42c1
NyatiZw <appsmaid19@gmail.com>
2023-03-28 18:22:45 +0000

    square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-"""Define class"""
+class Square():
+    size = 0
 
-
-class Square:
-    """Interstate Square
-    Attributes:
-        __size (int): size of square
-    """"
-    def __init__(self, size):
-        """Initialize square
-        Args:
-            size (int): size of square
-        Returns: None
-        """
-        self.__size = size
+sq = Square()
+print("{}".format(sq.size))
