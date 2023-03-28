2ca23daf170edcc10a11c46f04716c002aa384bc
NyatiZw <appsmaid19@gmail.com>
2023-03-28 20:10:03 +0000

    safe printing of intrgers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    while True:
-        try:
-            return True
-        except:
-            print("{:d}".format(value, end=""))
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
