b9814e96319667cbef4f2fb967a63aef95af7534
NyatiZw <appsmaid19@gmail.com>
2023-04-05 16:08:44 -0700

    area and perimeter of rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
 
     def __str__(self):
         """returns a string form of rectangle"""
-        if self.__width != 0 and sefl.__height != 0:
+        if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
                     for j in range(self.__height))
         return string
