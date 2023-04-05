899c7916cf05e77a96b74354c027ade8715de674
NyatiZw <appsmaid19@gmail.com>
2023-04-05 15:19:18 -0700

    area and perimeter of rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,8 +5,16 @@
 
 class Rectangle:
     """Representation of rectangle"""
+
     def __init__(self, width=0, height=0)
-        """Initialize rectangle"""
+        """Initialize rectangle
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+        Raises:
+            TypeError: if not integer
+            ValueError: if size is less than zero
+        """
         self.width = width
         self.height = height
 
@@ -34,7 +42,16 @@ class Rectangle:
                 """setter for height"""
                 if type(value) is not int:
                     raise TypeError("height must be an integer")
-                elif value < 0:
+                if value < 0:
                     raise ValueError("height must be >= 0")
-                else:
-                    self.__height = value
+                self.__height = value
+
+            def area(self):
+                """returns area of rectangle"""
+                return self.__width * self.__height
+
+            def perimeter(self):
+                """returns perimeter of rectangle"""
+                if self.__width == 0 or self.__height == 0:
+                    return 0
+                return (self.__width * 2) * (self.__height * 2)
