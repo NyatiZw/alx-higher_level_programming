368c71d882cb018805d0292bb265051c486fbcce
NyatiZw <appsmaid19@gmail.com>
2023-04-07 11:07:26 -0700

    test driven development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -25,9 +25,9 @@ def say_my_name(first_name, last_name=""):
     type_error_message_lname = "last_name must be a string"
 
     if not isinstance(first_name, str):
-        raise: TypeError(type_error_message_fname)
+        raise TypeError(type_error_message_fname)
 
     if not isinstance(last_name, str):
-        raise: TypeError(type_error_message_lname)
+        raise TypeError(type_error_message_lname)
 
     print("My name is {} {}".format(first_name, last_name))
