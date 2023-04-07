ffa9800dd06f77d4ee3d90caae21bbcaf6ee7d41
NyatiZw <appsmaid19@gmail.com>
2023-04-07 12:09:17 -0700

    Test Driven Development

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
-"""Function that prints a square"""
+"""
+Function that prints a square
+
+"""
 
 
 def print_square(size):
+
     """
     Function that prints a square
 
@@ -19,10 +23,11 @@ def print_square(size):
 
     """
 
-    for i in range(size):
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        elif size not isinstance(size, int):
-            raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
+    if size not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    for j in range(size):
         print("#" * size)
