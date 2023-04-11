8ba4ea9ad9310763f1117ae1d8aaf6d4b33a04cd
NyatiZw <appsmaid19@gmail.com>
2023-04-11 06:16:16 -0700

    Test driven development

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+
+Function that returns list of available attributes and methods of object
+"""
 
 def lookup(obj):
+    """ Function that returns object attributes
+
+    Args:
+        obj: object element
+
+    Returns:
+        Object attributes
+    """
+
     return (dir(obj))
