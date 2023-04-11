d9b8e2e65fd5e6ddbdc84f275597bd40f742d2b9
NyatiZw <appsmaid19@gmail.com>
2023-04-11 11:07:00 -0700

    python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,14 +4,11 @@
 Class MyList that inherits from list
 """
 class list:
-    def __init__(self, list):
-        self.list[] = int(list)
+    def __init__(self):
+        self.list = list
 
 class MyList(list):
     def __init__(self, list):
         return
 
         list.__init__(self,list)
-
-def print_sorted(self):
-    MyList.sort()
