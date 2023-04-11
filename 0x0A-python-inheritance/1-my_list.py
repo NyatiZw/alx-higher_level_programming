56986c7efc2e9d9c4bca911cb32168d1be60e206
NyatiZw <appsmaid19@gmail.com>
2023-04-11 10:06:00 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+Class MyList that inherits from list
+"""
+def print_sorted(self):
+    class list:
+        def __init__(self):
+            return int
+
+    class MyList(list):
+        def __init__(self, list):
+            return True
