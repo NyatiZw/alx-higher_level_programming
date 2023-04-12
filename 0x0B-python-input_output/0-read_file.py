fdd2730f22d4d5f5703cb873bdbead0f5ebde657
NyatiZw <appsmaid19@gmail.com>
2023-04-12 09:48:46 -0700

    python input output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Function to read text file
+
+"""
+
+
+def read_file(filename=""):
+    """Function to read text file
+
+    Args:
+        filename: files name
+
+    """
+
+    with open('', encoding="utf-8") as f:
+        read_file = f.read()
+
+    print("{}".format(read_file))
