0fea02ce43f2a22174bb2ac851ffa59545d27445
NyatiZw <appsmaid19@gmail.com>
2023-04-11 18:21:45 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 
+
+
+"""
+class BaseGeometry:
+    """ Empty class
+    
+    Raise: Exception
+    """
+
+    def __init__(self):
+        raise Exception("area() is not implemented")
+
