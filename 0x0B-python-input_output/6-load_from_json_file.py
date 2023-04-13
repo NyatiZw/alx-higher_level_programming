062accdc831ce4c4e9c3eb3661387de2f77d97bd
NyatiZw <appsmaid19@gmail.com>
2023-04-13 03:19:42 -0700

    python input output json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Function that creates an Object from a JSON file
+
+
+"""
+import json
+
+
+def load_from_json_file(filename):
+
+    """Function to write an object in jsonrepresentation
+
+    Args:
+        my_obj: object
+        filename: name of file
+
+    Return: text file
+
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return json.load(filename, f)
