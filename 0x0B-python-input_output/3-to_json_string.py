15dfc32188ec927abf23e729e65fe1fd19efc5ce
NyatiZw <appsmaid19@gmail.com>
2023-04-13 03:01:17 -0700

    python json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import json
 
 """
 Function that rturns the JSON representation of an object
 
+
 """
+import json
 
 
 def to_json_string(my_obj):
@@ -17,5 +18,4 @@ def to_json_string(my_obj):
     Return: json object
 
     """
-
-    return json.dump(my_obj)
+    return json.dumps(my_obj)
