7908469b21ad0ea28d2368a6afdb4508952c2bde
NyatiZw <appsmaid19@gmail.com>
2023-04-13 04:04:58 -0700

    python input output json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,22 +6,22 @@ Script that adds all arguments to a python list and saves as json
 
 """
 
+import sys
 
 if __name__ == "__main__":
 
     """Script to add arguments to json objectresentation"""
 
-    import sys
 
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file =
     __import__('6-load_from_json_file').load_from_json_file
 
-    fname = "add_item.json"
 
-    pylist = []
-    if len(sys.argv) >= 1:
-        pylist = sys.argv[1:]
+    try:
+        args = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        args = []
 
-    fname.extend(pylist)
-    save_to_json_file(pylist, fname)
+    args.extend(sys.argv[1:])
+    save_to_json_file(args, "add_item.json")
