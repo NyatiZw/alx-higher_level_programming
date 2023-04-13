87e1a133c48e2ca15f9b04603609c5ea56816457
NyatiZw <appsmaid19@gmail.com>
2023-04-13 03:53:42 -0700

    python input output json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Script that adds all arguments to a python list and saves as json
+
+
+"""
+import json
+import sys
+
+
+def add_item:
+
+    """Script to add arguments to json objectresentation"""
+
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    fname = ""
+
+    pylist = []
+    if len(sys.argv) >= 1:
+        pylist = sys.argv[1:]
+
+    fname.extend(pylist)
+    save_to_json_file(pylist, fname)
