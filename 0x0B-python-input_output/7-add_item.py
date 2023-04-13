d50157bfaf7e0186b4082bfd18885b6ed54643ed
NyatiZw <appsmaid19@gmail.com>
2023-04-13 04:10:11 -0700

    python input output json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -17,11 +17,15 @@ if __name__ == "__main__":
     load_from_json_file =
     __import__('6-load_from_json_file').load_from_json_file
 
-
+    fname = "add_item.json"
     try:
-        args = load_from_json_file("add_item.json")
+        args = load_from_json_file("fname")
     except FileNotFoundError:
         args = []
 
-    args.extend(sys.argv[1:])
-    save_to_json_file(args, "add_item.json")
+    pylist = []
+    if len(sys.argv) >= 1:
+        pylist = sys.argv[1:]
+
+    args.extend(pylist)
+    save_to_json_file(pylist,fname)
