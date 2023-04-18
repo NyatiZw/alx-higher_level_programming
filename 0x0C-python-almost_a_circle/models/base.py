302db499a822ba22887897eb503525f4ddb725d2
NyatiZw <appsmaid19@gmail.com>
2023-04-18 06:33:11 -0700

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 
 class Base:
-    """ First class named Base
-    Attributes:
-        __nb_objects : private class integer attribute
-    """
-
+    """ First class named Base """
     __nb_objects = 0
 
-
     def __init__(self, id=None):
-        """ Function to initialize a base
-        Args:
-            id : integer value of base id
-        """
+        """ Function to initialize a base """
         if id is not None:
             self.id = id
         else:
