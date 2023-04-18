c3650c1d2873ffab43e20466415b65c247908d92
NyatiZw <appsmaid19@gmail.com>
2023-04-18 06:24:25 -0700

    base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+
 class Base:
+    """ First class named Base
+    Attributes:
+        __nb_objects : private class integer attribute
+    """
+
     __nb_objects = 0
 
 
     def __init__(self, id=None):
-        self.id = id
-
-    if id is not None:
-        id = id
-    else:
-        __nb_objects += 1
-        id = __nb_objects
+        """ Function to initialize a base
+        Args:
+            id : integer value of base id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,6 @@ from .base import Base
 
 
 class Rectangle(Base):
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
@@ -12,42 +11,42 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-    def get_width(self):
-        return self.__width
+        def get_width(self):
+            return self.__width
 
-    def set_width(self, width):
-        if width <= 0:
-            raise ValueError('width must be > 0')
-        elif width is not type(int):
-            raise TypeError('width must be an integer')
-        self.__width = width
+        def set_width(self, width):
+            if width <= 0:
+                raise ValueError('width must be > 0')
+            elif width is not type(int):
+                raise TypeError('width must be an integer')
+            self.__width = width
 
-    def get_height(self):
-        return self.__height
+        def get_height(self):
+            return self.__height
 
-    def set_height(self, height):
-        if height <= 0:
-            raise ValueError('height must be > 0')
-        elif height is not type(int):
-            raise TypeError("height must be an integer")
-        self.__height = height
+        def set_height(self, height):
+            if height <= 0:
+                raise ValueError('height must be > 0')
+            elif height is not type(int):
+                raise TypeError("height must be an integer")
+            self.__height = height
 
-    def get_x(self):
-        return self.__x
+        def get_x(self):
+            return self.__x
 
-    def set_x(self, x):
-        if x < 0:
-           raise ValueError('x must be >= 0')
-        elif x is not type(int):
-            raise TypeError("x must be an integer")
-        self.__x = x
+        def set_x(self, x):
+            if x < 0:
+                raise ValueError('x must be >= 0')
+            elif x is not type(int):
+                raise TypeError("x must be an integer")
+            self.__x = x
 
-    def get_y(self):
-        return self.__y
+        def get_y(self):
+            return self.__y
 
-    def set(self, y):
-        if y < 0:
-            raise ValueError('y must be >= 0')
-        elif y is not type(int):
-            raise TypeError("y must be an integer")
-        self.__y = y
+        def set(self, y):
+            if y < 0:
+                raise ValueError('y must be >= 0')
+            elif y is not type(int):
+                raise TypeError("y must be an integer")
+            self.__y = y
