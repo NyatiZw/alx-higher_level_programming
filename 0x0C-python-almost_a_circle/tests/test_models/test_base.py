7901b319dea4035a5c9f2e39dd7da8d4b1239d72
NyatiZw <appsmaid19@gmail.com>
2023-04-18 07:21:34 -0700

    base tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """ Defines unittests for base class """
 
-import os
-import unittest
 from models.base import Base
+import unittest
 
 
 class TestBase_instantiation(unitttest.TestCase):
     """ Unittesting for base class """
+
+    def test_upper(self):
+        self.assertTrue(True)
+    def base_not_running(self):
+        print("no run")
