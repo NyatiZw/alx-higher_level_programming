eacc59e5576705b0dee0ec1c88b8a75c1573d9e3
NyatiZw <appsmaid19@gmail.com>
2023-04-18 07:04:45 -0700

    base tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines unittests for base class """
+
+import os
+import unittest
+from models.base import Base
+
+
+class TestBase_instantiation(unitttest.TestCase):
+    """ Unittesting for base class """
