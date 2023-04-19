b1e029af410f280fbff2a7fa6412f76c260c814e
NyatiZw <appsmaid19@gmail.com>
2023-04-19 08:04:52 -0700

    validaton setters and getters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -34,10 +34,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, val):
         """ width setter """
-        if val <= 0:
-            raise ValueError('width must be > 0')
         if type(val) != int:
             raise TypeError('width must be an integer')
+        if val <= 0:
+            raise ValueError('width must be > 0')
         self.__width = val
 
     @property
@@ -48,10 +48,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, val):
         """ height setter """
-        if val <= 0:
-            raise ValueError('height must be > 0')
         if type(val) != int:
             raise TypeError("height must be an integer")
+        if val <= 0:
+            raise ValueError('height must be > 0')
         self.__height = val
 
     @property
@@ -62,10 +62,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, val):
         """ x setter """
-        if val < 0:
-            raise ValueError('x must be >= 0')
         if type(val) != int:
             raise TypeError("x must be an integer")
+        if val < 0:
+            raise ValueError('x must be >= 0')
         self.__x = val
 
     @property
@@ -76,8 +76,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, val):
         """ y setter """
-        if val < 0:
-            raise ValueError('y must be >= 0')
         if type(val) != int:
             raise TypeError("y must be an integer")
+        if val < 0:
+            raise ValueError('y must be >= 0')
         self.__y = val
