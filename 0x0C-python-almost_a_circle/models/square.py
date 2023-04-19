ec77b6c85a0c3c6cbae80d7c3ee9353e9cc31cf4
NyatiZw <appsmaid19@gmail.com>
2023-04-19 10:13:27 -0700

    now the square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Class Square that inherits from rectangle """
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """ Intestaition of Square class """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Square Class """
+        self.size = size
+        self.x = x
+        self.y = y
+        super().__init__.Rectangle(id, x, y, width, height)
