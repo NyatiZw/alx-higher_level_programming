e39cd1d5b386b5ef6366651eda06c236b042a73d
NyatiZw <appsmaid19@gmail.com>
2023-04-19 17:18:00 -0700

    initializing arguemnets

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,8 +6,19 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ Intestaition of Square class """
     def __init__(self, size, x=0, y=0, id=None):
-        """ Square Class """
-        self.size = size
-        self.x = x
-        self.y = y
-        super().__init__.__Rectangle(id, x, y, width, height)
+        """ Square Class
+        Args:
+            size : size of square
+            x: element
+            y: member
+            id: square id
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        _id = self.id
+        _x = self.x
+        _y = self.y
+        _size = self.width, self.height
+
+        return "[Square] + ({}) {}/{} - {}".format(_id, _x, _y, _size)
