558efb35c753248b5557c579899de1b74cbef1e5
NyatiZw <appsmaid19@gmail.com>
2023-04-20 06:29:02 -0700

    adding json static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Define class model base """
-
+import json
 
 class Base:
     """ First class named Base
@@ -20,3 +20,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries = 0:
+            return ('[]')
+        return json.list_dictionaries
+
