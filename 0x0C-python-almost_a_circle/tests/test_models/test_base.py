e84859ef729a3332671a78df3bfe8f389725bc92
NyatiZw <appsmaid19@gmail.com>
2023-04-21 00:39:19 -0700

    unittesting base model

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,19 +12,19 @@ from models.base import Base
 class TestBase_instantiation(unittest.TestCase):
     """ Unittesting for base class """
     def test_no_args(self):
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1)
+        _b1 = Base()
+        _b2 = Base()
+        self.assertEqual(_b1.id, _b2.id - 1)
 
     def test_none_id(self):
-        b1 = Base(None)
-        b2 = Base(None)
-        self.assertEqual(b1.id, b2.id - 1)
+        _b1 = Base(None)
+        _b2 = Base(None)
+        self.assertEqual(_b1.id, _b2.id - 1)
 
     def test_public_id(self):
-        b1 = Base(7)
-        b1.id = 1
-        self.assertEqual(1, b1.id)
+        _b1 = Base(7)
+        _b1.id = 1
+        self.assertEqual(1, _b1.id)
 
     def test_string_id(self):
         self.assertEqual("World", Base("World").id)
@@ -33,6 +33,6 @@ class TestBase_instantiation(unittest.TestCase):
         self.assertEqual(range(5), Base(range(5)).id)
 
     def test_NaN(self):
-        b1 = Base()
-        b1.id = "#"
-        self.assertEqual("#", b1.id)
+        _b1 = Base()
+        _b1.id = "#"
+        self.assertEqual("#", _b1.id)
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import io
+import sys
+from models.rectangle import Rectangle
+
+class TestRectangle_instantiation(unittest.TestCase):
+    
