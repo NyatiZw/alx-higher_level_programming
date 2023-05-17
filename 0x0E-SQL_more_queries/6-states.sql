b8c48f67b00c65f8632489c5aa0ef7e6589c6d75
NyatiZw <appsmaid19@gmail.com>
2023-05-17 16:55:24 -0700

    listing table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,12 +1,11 @@
 -- A script 
 -- that creates database and table
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Use the database
-USE 'hbtn_0d_usa';
+USE hbtn_0d_usa;
 
--- create a table in the database
-CREATE TABLE IF NOT EXISTS 'states' (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 	);
