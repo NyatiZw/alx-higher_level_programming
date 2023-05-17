f012dadcf01cf577fc24b3392bc45e0aad159399
NyatiZw <appsmaid19@gmail.com>
2023-05-17 16:02:42 -0700

    creating db and tble

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- A script 
 -- that creates database and table
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-USE 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'cities' (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
-	states_id INT NOT NULL FOREGIN KEY,
+	states_id INT NOT NULL,
 	FOREIGN KEY (states_id) REFERENCES states(id)
 	);
