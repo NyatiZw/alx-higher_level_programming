9179d3362471f7a7b91ee8ffe41c95495687b1d0
NyatiZw <appsmaid19@gmail.com>
2023-05-17 15:07:50 -0700

    granting permissions

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- A script 
 -- to create the database and user
-
 CREATE DATABASE IF NOT EXISTS 'hbtn_0c_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'hbtn_0c_2' IDENTIFIED BY 'user_od_2_pwd';
 GRANT SELECT ON 'hbtn_0c_2' TO 'user_0d_2';
