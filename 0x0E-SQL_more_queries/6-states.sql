10baf67f2e869e0c1f60656a1868f225d6b25a46
NyatiZw <appsmaid19@gmail.com>
2023-05-17 15:45:13 -0700

    creating db and tble

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,11 @@
 -- A script 
 -- that creates database and table
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+
+-- Use the database
 USE 'hbtn_0d_usa';
+
+-- create a table in the database
 CREATE TABLE IF NOT EXISTS 'states' (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
