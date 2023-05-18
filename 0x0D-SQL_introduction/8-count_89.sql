0c268b1590a7976dbb952193267500152a363f27
NyatiZw <appsmaid19@gmail.com>
2023-05-18 06:20:17 -0700

    corrections

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,7 +1,6 @@
 -- A script
 -- that displays the number of recods with id=89 in first_table
-SET @dbase = 'hbtn_0c_0';
-USE @dbase;
+USE hbtn_0c_0;
 SELECT COUNT(*) AS _count
 FROM 'first_table'
 WHERE id = 89;
