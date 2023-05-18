926bf615ae7d5bf02e1cd48049dcbf0b71b4709f
NyatiZw <appsmaid19@gmail.com>
2023-05-18 06:11:24 -0700

    corrections

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,18 +1,4 @@
 -- A script
 -- that creates a new table in a database
-SET @dbase = 'hbtn_0c_0';
-USE @dbase;
-SET @tblname = (
-	SELECT TABLE_NAME
-	FROM INFORMATION_SCHEMA.TABLES
-	WHERE TABLE_SCHEMA = @dbase
-	LIMIT 1
-);
-
-IF @tblname IS NOT NULL THEN
-	SET @res = CONCAT('SELECT * FROM ', @dbase, '.', @tblname, ';');
-
-	PREPARE statement FROM @res;
-	EXECUTE statement;
-	DEALLOCATE PREPARE statement;
-END IF;
+USE hbtn_0c_0;
+SELECT first_table LIMIT 1;
