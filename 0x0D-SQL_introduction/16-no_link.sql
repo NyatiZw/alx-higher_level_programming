e1df933fa9872d8a791e23bface98a7e095dfa78
NyatiZw <appsmaid19@gmail.com>
2023-05-18 10:27:12 -0700

    corrections

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,6 @@
 -- A Script
 -- to remove all records with score <=5 in second_table
-SELECT *
-FROM second_table;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
