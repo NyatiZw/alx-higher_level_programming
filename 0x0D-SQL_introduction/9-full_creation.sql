52e321c7987115c05e696083e80389d7412c257b
NyatiZw <appsmaid19@gmail.com>
2023-05-18 09:59:24 -0700

    corrections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,11 @@
--- A script
--- that creates second_table in database hbtn_0c_0
+-- A script that creates second_table in database hbtn_0c_0
+-- And adds new rows
 CREATE TABLE IF NOT EXITS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT);
 
--- Insert rows into second_table
-INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10),
-INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3),
-INSERT INTO second_table(id, name, score) VALUES (3, 'Bob', 14),
+INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table(id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO second_table(id, name, score) VALUES (4, 'George', 8);
