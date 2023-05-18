f6af5af9da615e26bdceb315defd656c8ab596cf
NyatiZw <appsmaid19@gmail.com>
2023-05-18 07:36:12 -0700

    corrections

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,10 +1,4 @@
 -- A Script
 -- to remove all records with score <=5 in second_table
-SET @dbname := 'hbtn_0c_0';
-SET @query := CONCAT(
-	'DELETE FROM ', @dbname, '.second_table WHERE score <= 5;'
-);
-
-PREPARE statement FROM @query;
-EXECUTE statement;
-DEALLOCATE PREPARE statement;
+USE hbtn_0c_0;
+DELETE FROM hbtn_0c_0.second_table WHERE score <= 5;
