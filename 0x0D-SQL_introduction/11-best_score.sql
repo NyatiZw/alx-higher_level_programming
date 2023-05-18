a8082514da013a504d9761f34797561b1809f656
NyatiZw <appsmaid19@gmail.com>
2023-05-18 06:39:57 -0700

    corrections

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,10 +1,6 @@
 -- A Script
 -- to list all records with score >= 10 in second_table
-SET @dbname := 'hbtn_0c_0';
-SET @query := CONCAT(
-	'SELECT score, name FROM ', @dbname, '.second_table WHERE score >= 10 ORDER BY score DESC;'
-);
-
-PREPARE statement FROM @query;
-EXECUTE statement;
-DEALLOCATE PREPARE statement;
+SELECT score, name
+FROM hbtn_0c_0.second_table
+WHERE score >= 10
+ORDER BY score DESC;
