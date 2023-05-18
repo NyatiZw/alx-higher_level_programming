3fb498c25294d1a050041997cc28306048e97685
NyatiZw <appsmaid19@gmail.com>
2023-05-18 07:46:19 -0700

    corrections

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- A Script
 -- to compute the score average of all records in the table
 USE hbtn_0c_0;
-CONCAT('SELECT AVG(score) AS average FROM 'hbtn_0c_0 '.scond_table');
+SELECT AVG(score) AS average FROM 'hbtn_0c_0.second_table';
