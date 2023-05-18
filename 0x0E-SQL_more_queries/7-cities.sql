db57a57423cd214602347cf6f174512233e205f6
NyatiZw <appsmaid19@gmail.com>
2023-05-17 17:00:22 -0700

    listing table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,9 @@
 -- that creates database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+CREATE TABLE IF NOT EXISTS unique_id cities (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
 	states_id INT NOT NULL,
 	FOREIGN KEY (states_id) REFERENCES states(id)
