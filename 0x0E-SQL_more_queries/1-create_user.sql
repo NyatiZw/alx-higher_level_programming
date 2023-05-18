be6155382b61c4d429f85b91c89efb4b6e8f32d1
NyatiZw <appsmaid19@gmail.com>
2023-05-18 16:15:29 -0700

    corrections

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- A script
 -- that creates the MySQL server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'host' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'host' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'host';
+FLUSH PRIVILEGES;
