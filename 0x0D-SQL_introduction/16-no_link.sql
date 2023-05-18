10d10a9ba72ef9ad34e8a3f655ddf3679931cc40
NyatiZw <appsmaid19@gmail.com>
2023-05-18 08:03:13 -0700

    corrections

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,10 +1,6 @@
 -- A Script
 -- to remove all records with score <=5 in second_table
-SET @dbname := 'hbtn_0c_0';
-SET @query := CONCAT(
-	'SELECT score, name FROM ', @dbname, '.second_table WHERE name IS NOT NULL AND name <> "" ORDER BY score DESC;'
-);
+USE hbtn_0c_0;
 
-PREPARE statement FROM @query;
-EXECUTE statement;
-DEALLOCATE PREPARE statement;
+SELECT *
+FROM second_table;
