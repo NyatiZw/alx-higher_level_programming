cf86d82cc016522dd101913c6a61024b12a07f18
NyatiZw <appsmaid19@gmail.com>
2023-05-18 10:09:37 -0700

    corrections

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
 -- A Script
 -- to update the score of Bob without using id value
-USE hbtn_0c_0;
-UPDATE hbtn_0c_0.second_table SET score = 10 WHERE name = "Bob";
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
 
