94157c2aa362f76e275811f00da1bb497f7a0364
NyatiZw <appsmaid19@gmail.com>
2023-05-18 07:50:56 -0700

    corrections

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,4 @@
 -- A Script
 -- to compute the score average of all records in the table
-SET @dbname := 'hbtn_0c_0';
-SET @query := CONCAT(
-	'SELECT score, name FROM ', @dbname, '.second_table WHERE name <> "" ORDER BY score DESC;'
-);
+USE hbtn_0c_0;
+SELECT score, name FROM second_table WHERE name <> "" ORDER BY score DESC;
