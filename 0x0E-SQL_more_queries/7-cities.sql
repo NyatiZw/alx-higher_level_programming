21fde611164c4964e546da1b524511d571bb4233
NyatiZw <appsmaid19@gmail.com>
2023-05-17 17:05:19 -0700

    listing table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,7 +5,7 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	states_id INT NOT NULL,
-	FOREIGN KEY (states_id) REFERENCES states(id)
-	name VARCHAR(256) NOT NULL,
+	states_id INT NOT NULL
+	FOREIGN KEY (states_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
 	);
