e887ef8f06dadd91eda66411b4836acfb08d215f
NyatiZw <appsmaid19@gmail.com>
2023-05-18 06:36:35 -0700

    corrections

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,9 +1,7 @@
 -- A Script
 -- to list all records of the table second_table
 
-SET @dbase := 'hbtn_0c_0';
-SET @query := CONCAT('SELECT score, name FROM ', @dbase, '.second_table ORDER BY score DESC;'
-);
-PREPARE statement FROM @query;
-EXECUTE statement;
-DEALLOCATE PREPARE statement;
+USE hbtn_0c_0;
+SELECT score, name
+FROM hbtn_0c_0
+ORDER BY score DESC;
