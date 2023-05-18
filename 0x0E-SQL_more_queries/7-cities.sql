661ff7d6233c998afe69f93a17f3d7ea265e26eb
NyatiZw <appsmaid19@gmail.com>
2023-05-18 16:55:28 -0700

    corrections

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- A script 
 -- that creates database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY(id),
- states_id INT NOT NULL,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+ state_id INT NOT NULL,
  name VARCHAR(256) NOT NULL,
- FOREIGN KEY (states_id) REFERENCES states(id)
+ FOREIGN KEY (state_id) REFERENCES state(id)
 );
