a19d1347c329382af1af514fd8de1fd7056e0a54
NyatiZw <appsmaid19@gmail.com>
2023-05-18 06:55:01 -0700

    corrections

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,10 +1,5 @@
 -- A Script
 -- to update the score of Bob without using id value
-SET @dbname := 'hbtn_0c_0';
-SET @query := CONCAT(
-	'UPDATE ', @dbname, '.second_table SET score = 10 WHERE name = "Bob";'
+USE hbtn_0c_0;
+CONCAT('UPDATE ', hbtn_0c_0, '.second_table SET score = 10 WHERE name = "Bob";'
 );
-
-PREPARE statement FROM @query;
-EXECUTE statement;
-DEALLOCATE PREPARE statement;
