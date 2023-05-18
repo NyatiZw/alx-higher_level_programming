6fd2e69845caaa634dbd834c47eb6bce70a4bec1
NyatiZw <appsmaid19@gmail.com>
2023-05-18 06:03:46 -0700

    corrections

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,9 +1,4 @@
 -- A script
 -- that prints full description of table
-SET @dbase = 'hbtn_0c_0';
-SET @tblname = 'first_table';
-
-SET @query = CONCAT('SELECT * FROM information_schema.columns WHERE table_schema = "', @dbase, '" AND table_name = "', @tblname, '";');
-
-PREPARE staatement FROM @query;
-EXECUTE statement;
+USE hbtn_0c_0;
+SHOW COLUMNS FROM first_table;
