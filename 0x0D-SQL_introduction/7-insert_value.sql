f843095016c86338cfea57b2f7d7cb5607ebbc95
NyatiZw <appsmaid19@gmail.com>
2023-05-18 06:18:27 -0700

    corrections

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,9 +1,8 @@
 -- A script
 -- that inserts a new row in the table first_table
-SET @dbase = 'hbtn_0c_0';
-USE @dbase;
+USE hbtn_0c_0;
 SET @id_val = 89;
-SET @name_val = 'Best School';
+SET @name_val = "Best School";
 
-INSERT INTO 'first_table' (id, name)
-VALUES(@id_val, @name_val);
+-- Inserting the values
+INSERT INTO first_table (id_val, name_val)
