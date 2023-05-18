04fa348201f82378d034abc4c028225ea89d8179
NyatiZw <appsmaid19@gmail.com>
2023-05-18 06:32:45 -0700

    corrections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,16 +1,15 @@
 -- A script
 -- that creates second_table in database hbtn_0c_0
-USE hbtn_0c_0
-SET @tble = (SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'hbtn_0c_0' table_name = 'second_table');
-IF @tble = 0 THEN
-	CREATE TABLE second_table (
-		id INT,
-		name VARCHAR(256),
-		score INT
-	);
-END IF;
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXITS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
 
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+-- Insert rows into second_table
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
