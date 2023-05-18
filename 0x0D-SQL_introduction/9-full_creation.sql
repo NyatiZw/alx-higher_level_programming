cf60e2a1f625e57daa7fd1cbad5b40c73f75b830
NyatiZw <appsmaid19@gmail.com>
2023-05-18 10:00:48 -0700

    corrections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,8 @@
 CREATE TABLE IF NOT EXITS second_table (
 	id INT,
 	name VARCHAR(256),
-	score INT);
+	score INT
+);
 
 INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
