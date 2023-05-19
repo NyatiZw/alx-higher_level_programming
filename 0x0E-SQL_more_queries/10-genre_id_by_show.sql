7f918561e616469baf3c3a0e962e606f1f7e271e
NyatiZw <appsmaid19@gmail.com>
2023-05-18 19:47:56 -0700

    corrections

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,9 @@
 -- A script
 -- that lists linked tables
+USE download;
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM hbtn_0d_tvshows
 JOIN tv_shows ON hbtn_0d_tvshows.tv_show_id = tv_shows.id
 JOIN tv_show_genres ON hbtn_0d_tvshows.tv_show_genre_id = tv_show_genres.id
-WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
