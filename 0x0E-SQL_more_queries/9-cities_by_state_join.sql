69a5b3ba48ff3d8ba1e9ab29c34184350d22fe9f
NyatiZw <appsmaid19@gmail.com>
2023-05-18 18:26:02 -0700

    corrections

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- A script
--- that lists all specific entries in database
+-- that lists all cities contained in database
 
-USE hbtn_0d_usa;
-SHOW TABLES.states.id ASC;
+SELECT DISTINCT cities.id, cities.name, states.name
+FROM cities, states
+ORDER BY ASC;
