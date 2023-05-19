0d30c3ab9acb00ef5d662227b8c3d244e99f938d
NyatiZw <appsmaid19@gmail.com>
2023-05-18 17:52:51 -0700

    corrections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,7 @@
 --A script that creates second_table in database
 --and adds new rows
-USE hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS second_table (
  id INT,
  name VARCHAR(256),
