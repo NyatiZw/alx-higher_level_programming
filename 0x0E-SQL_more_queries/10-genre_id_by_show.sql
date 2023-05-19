de25fc607f483b3382875c7980c4e1c5ab83c54c
NyatiZw <appsmaid19@gmail.com>
2023-05-18 19:53:17 -0700

    corrections

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,6 +4,6 @@ USE download;
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM hbtn_0d_tvshows
-JOIN tv_shows ON hbtn_0d_tvshows.tv_show_id = tv_shows.id
-JOIN tv_show_genres ON hbtn_0d_tvshows.tv_show_genre_id = tv_show_genres.id
+JOIN tv_shows ON tv_show_id = tv_shows.id
+JOIN tv_show_genres ON tv_show_genre_id = tv_show_genres.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
