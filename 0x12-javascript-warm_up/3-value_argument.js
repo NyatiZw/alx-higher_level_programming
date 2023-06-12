6066634e6d834b100c4a9cf8f5cd6a8f5e95bf74
NyatiZw <appsmaid19@gmail.com>
2023-06-12 11:57:21 -0700

    Javascript arguments

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+if (argv[2] === undefined) {
+  console.log("No argument");
+} else {
+	const firstArgument = argv[2];
+	console.log(firstArgument);
+}
