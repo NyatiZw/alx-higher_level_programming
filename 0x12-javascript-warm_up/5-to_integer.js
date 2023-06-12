dc480020a367163b787427a717783422c9c7fe15
NyatiZw <appsmaid19@gmail.com>
2023-06-12 13:25:35 -0700

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const firstArgument = argv[2];
+
+if (isNaN(firstArgument)) {
+	console.log("Not a number");
+} else {
+	console.log(`My number: ${firstArgument}`);
+}
