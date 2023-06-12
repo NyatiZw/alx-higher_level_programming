caa59b4f25a018562fca0ccb08567aa12c4f8669
NyatiZw <appsmaid19@gmail.com>
2023-06-12 13:52:12 -0700

    Javascript loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const a = "C is fun";
+const b = "Python is cool";
+const c = "JavaScript is amazing";
+const lang = [`${a}`, `${b}`, `${c}`];
+
+for (let i = 0; i < lang.length; i++) {
+	console.log(lang[i]);
+}
