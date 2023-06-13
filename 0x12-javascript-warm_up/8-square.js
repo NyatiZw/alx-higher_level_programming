3fb163cebd267c93ef97fd8f50305b773ecf7dff
NyatiZw <appsmaid19@gmail.com>
2023-06-12 17:35:31 -0700

    creating a square in js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/* 
+ * Script to print an integer
+ */
+
+const { argv } = require('process');
+const size = argv[2];
+const a = "x";
+
+if (isNaN(size)) {
+	console.log("Missing size");
+} else {
+	let count = +size;
+	let line = 0;
+	while (count > line) {
+		let row = "";
+		for (let i = 0; i < count; i++) {
+			row += a;
+		}
+		console.log(row);
+		line++;
+	}
+}
