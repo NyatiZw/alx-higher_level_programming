79215d8e07ff62b73eb3927680c14eb8a9f4d1e2
NyatiZw <appsmaid19@gmail.com>
2023-06-12 18:02:41 -0700

    adding variables through a fuunction in js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+const a = Number(argv[2]);
+const b = Number(argv[3]);
 
+function add(a, b) {
+	return a + b;
+}
+
+console.log(add(a, b));
