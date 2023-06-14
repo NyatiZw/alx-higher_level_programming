9b70a975c59bdddf3cf507249214cb96347fc4de
NyatiZw <appsmaid19@gmail.com>
2023-06-13 23:26:01 -0700

    pushing rectangle class 1

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
 // Class definition of a rectangle
 
-class Rectangle {
-  width: number;
-  height: number;
-
-
-  constructor(w: number, h: number) {
-    this.width = w;
-    this.height = h;
-  }
-}
+class Rectangle {}
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Class definition of a rectangle
+
+class Rectangle {
+  width: number;
+  height: number;
+
+
+  constructor(w: number, h: number) {
+    this.width = w;
+    this.height = h;
+  }
+}
