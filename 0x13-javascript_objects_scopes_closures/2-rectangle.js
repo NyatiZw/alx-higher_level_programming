5fa339375547b606dba9dde14ccaa5e3cad2f634
NyatiZw <appsmaid19@gmail.com>
2023-06-14 09:41:04 -0700

    pushing if condition to class

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,8 +4,8 @@
 module.exports = class Rectangle {
 	constructor (w, h) {
 		if ((w % 1 !== 0) || (h % 1 !== 0) || w <= 0 || h <= 0) {
-		this.width = w;
-		this.height = h;
+			this.width = w;
+			this.height = h;
 		}
 	}
 };
