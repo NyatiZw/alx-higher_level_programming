15027000de56ec49c120f108a7f3fd42955964ef
NyatiZw <appsmaid19@gmail.com>
2023-06-14 12:51:46 -0700

    Convert to different base

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,10 +2,6 @@
 
 exports.converter = function (base) {
   return function convertToBase (number) {
-    if (number < base) {
-      return number.toString();
-    } else {
-      return convertToBase(Math.floor(number / base)) + (number % base).toString();
-    }
+    return number < base ? number.toString() : convertToBase(number / base | 0) + (number % base).toString();
   };
 };
