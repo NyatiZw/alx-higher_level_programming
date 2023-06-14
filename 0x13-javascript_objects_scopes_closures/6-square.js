f0b3ce6cb27f85b1557fc7d5ac83b667268344cd
NyatiZw <appsmaid19@gmail.com>
2023-06-14 11:35:14 -0700

    pushing square 0

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const SquareOriginal = require('./5-square');
+class Square extends SquareOriginal {
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('c'.repeat(this.width));
+      }
+    }
+  }
+}
+
+
+module.exports = squareOriginal;
