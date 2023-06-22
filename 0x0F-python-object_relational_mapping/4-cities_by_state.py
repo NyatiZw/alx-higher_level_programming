ab5f7dd0bb06fcd944c04745b9361a7854e5b944
NyatiZw <appsmaid19@gmail.com>
2023-06-22 14:32:29 -0700

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Python Script to list all states from a database"""
+
+import sys
+import MySQLdb
+
+
+def list_cities(mysql_username, mysql_password, database_name):
+    # Establish a connection to the MySQL database
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            charset="utf8"
+    )
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the SQL query to fetch all states
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all the rows returned by query
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the database connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Error in your arguments")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
+        list_cities(username, password, dbname)
