55700121f079b02fcb30bbb22dedb71d2f5dc8b9
NyatiZw <appsmaid19@gmail.com>
2023-06-23 18:51:33 -0700

    corrections

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,10 +3,9 @@
 of a State and instance declarative_base()
 """
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql://localhost:3306')
 Base = declarative_base()
 
 
@@ -17,6 +16,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
