e2d64f6a10557d65a0096e0e64f681338790a318
NyatiZw <appsmaid19@gmail.com>
2023-06-29 15:56:58 -0700

    corrections

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to send a DELETE request to a URL and display the body of the body of the response
-curl -s -X DELETE "$1" | echo 'I am a DELETE request'
+curl -s -X DELETE "$1"
