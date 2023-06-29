bfd0b0e7eb530c17037545d4bda5675a1fa340ec
NyatiZw <appsmaid19@gmail.com>
2023-06-29 07:29:59 -0700

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that takes URL and displays all HTTO methods
 
-curl -s -X OPTIONS -I 0.0.0.0:5000 | awk -F': ' '/^Allow/ { print $2 }'
+curl -s -X METHODS -I 0.0.0.0:5000 | awk -F': ' '/^Allow/ { print $2 }'
