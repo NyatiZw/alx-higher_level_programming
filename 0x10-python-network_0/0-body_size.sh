fc95c9153721c3bc19ee3ba06d6f781f1aa6ea5f
NyatiZw <appsmaid19@gmail.com>
2023-06-29 05:36:43 -0700

    curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Script that takes in a URL, sends Request and displays size of the body Response
 
-URL="$1" # Get the URL from command line
-
-# Send the request and store the response in a variable
-response=$(curl -sI "$URL")
-
-# Extract Content-Length header
-content_length=$(echo "$response" | awk '/Content-Length/ { print $2 }')
-
-# Display the size in bytes
-echo "$content_length"
+curl -sI 0.0.0.0:5000
