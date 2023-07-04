522a3f3a47b2de9362f73332917a2933ddabc5a2
NyatiZw <appsmaid19@gmail.com>
2023-07-04 05:38:02 -0700

    response header value

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,8 +3,7 @@
 Script to send a Post request with emailand display body response
 """
 
-import urllib.request
-import urllib.parse
+import requests
 from sys import argv
 
 
@@ -12,11 +11,8 @@ if __name__ == "__main__":
     url = argv[1]
     email = {'email': argv[2]}
 
-    data = urllib.parse.urlencode(email).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
+    data = {"email": email}
+    response = requests.post(url, data=data)
 
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-
-    print("Your email is: ")
-    print(body)
+    print("Response body:")
+    print(response.text)
