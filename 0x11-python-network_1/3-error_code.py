13b38639ab74753001c6ceb66928f9faed2db4cb
NyatiZw <appsmaid19@gmail.com>
2023-07-04 03:44:10 -0700

    Error code 0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL, sends a request to the URL and 
+Write a Python script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8).
 """
 from sys import argv
 import urllib.request
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
         with urllib.request.urlopen(argv[1]) as response:
             print(response.read().decode('utf-8'))
