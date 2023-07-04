beed4466d230e4594960e90762f0357f99706d96
NyatiZw <appsmaid19@gmail.com>
2023-07-04 04:14:28 -0700

    Whats my status 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,18 +6,16 @@ Script to fetch https resources
 
 from requests import get
 
-url = "https://alx-intranet.hbtn.io/status"
-
-res = get(url)
-body = res.text
-
-res_type = type(body).__name__
-formatted_body = f"- type: {res_type}"
-
-print("Body response:")
-print(formatted_body)
-print ("- content:", body)
-
 
 if __name__ == "__main__":
-    pass
+    url = "https://alx-intranet.hbtn.io/status"
+
+    res = get(url)
+    body = res.text
+
+    res_type = type(body).__name__
+    formatted_body = f"- type: {res_type}"
+
+    print("Body response:")
+    print(formatted_body)
+    print ("- content:", body)
