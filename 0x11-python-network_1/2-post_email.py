03c178392586b3947718c833f0b2d832217a43fd
NyatiZw <appsmaid19@gmail.com>
2023-07-04 03:28:02 -0700

    Post an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,18 +5,18 @@ Script to send a Post request with emailand display body response
 
 import urllib.request
 import urllib.parse
-import sys
-
-url = sys.argv[1]
-email = sys.argv[2]
+from sys import argv
 
 
 if __name__ == "__main__":
-    data = urllib.parse.urlencode({'email: email'}).encode('utf-8')
-    req = urllib.request.Request(url, datad=data, method='POST')
+    url = argv[1]
+    email = {'email': argv[2]}
+
+    data = urllib.parse.urlencode(email).encode('utf-8')
+    req = urllib.request.Request(url, data, method='POST')
 
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
 
-    print("Response body:")
+    print("Your email is:")
     print(body)
