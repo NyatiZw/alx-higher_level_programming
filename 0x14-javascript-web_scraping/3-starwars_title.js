95e84bc59b28d766199b9f7eb00bbbcce0e30e30
NyatiZw <appsmaid19@gmail.com>
2023-07-26 11:12:14 +0000

    initial commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// Script that writes a string to a file
+
+const request = require('request');
+
+function getMovieTitle(movieId) {
+  const apiUrl = 'https://swapi-api.alx-tools.com/api/films/${movieId}';
+
+  request.get(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else if (response.statusCode !== 200) {
+      console.error(error)
+    } else {
+      const movieData = JSON.parse(body);
+      console.log(movieData.title);
+    }
+  });
+}
+
+// Check if the file path argument is provided
+if (process.argv.length < 3) {
+  console.error(error);
+} else {
+  const movieId = process.argv[2];
+  getMovieTtile(movieId);
+}
