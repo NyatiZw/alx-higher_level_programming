fb78364223deae0a1ca999ce7dcd6b2d2f5e1b08
NyatiZw <appsmaid19@gmail.com>
2023-08-02 02:17:15 -0700

    click and turn red

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+//Change the text color using jquery
+$(document).ready(function(){
+	$("header").css((color:"#FF0000"});
+});
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,4 @@
+//Updates text color of header
+$("#red_header").click(function(){
+	$("header").css({color: "#FF0000"});
+});
