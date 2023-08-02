de9c21e329ad6deb73f11ec84043756d0cf9d62b
NyatiZw <appsmaid19@gmail.com>
2023-08-02 16:25:54 -0700

    corrections

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,8 +1,8 @@
 // Use the jquery API to fetch the character data from URL
-$.getJson('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data, textStatus) {
 	// Extract the character name from the fetched data
 	const characterName = data.name;
 
 	// Update <div> text with character
-	$('#character').text(characterName);
+	$('DIV#character').text(data.name);
 });
